ea1d0ad2ac7056a8c6b401c5aa6244f1650ae39f
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2015-06-28 12:01:49 -0500

    Feature: Navigation is aware of logged in user
    LocalStorage keeps track of logged in user between browser refreshes.
    Logout works properly in both desktop and mobile navs.

--- src/cljs/reagent_batteries_included_token_auth/navigation.cljs
+++ src/cljs/reagent_batteries_included_token_auth/navigation.cljs
@@ -1,11 +1,10 @@
 (ns reagent-batteries-included-token-auth.navigation
   (:require [reagent.core  :refer [atom]]
+            [reagent-batteries-included-token-auth.shared-state :refer [auth-creds-ratom]]
+            [alandipert.storage-atom :refer [remove-local-storage!]]
             [secretary.core :as secretary :include-macros true]))
 
 (def nav-state (atom {:mobile-menu-visiable false :active-route ""}))
-
-;TODO move this to something global?
-(def global (atom {:example-token ""}))
 
 (defn mobile-nav-click [route active-name]
   (swap! nav-state assoc :mobile-menu-visiable (not (:mobile-menu-visiable @nav-state)))
@@ -15,7 +14,16 @@
 (defn active-route? [route-name]
   (if (= route-name (:active-route @nav-state)) "active" ""))
 
-(defn auth-nav-desktop []
+(defn auth-nav-logged-in []
+  [:ul {:class "nav navbar-nav navbar-right"}
+   [:li {:class "dropdown"}
+    [:a {:class "dropdown-toggle" :data-toggle "dropdown"} "Logout" [:b {:class "caret"}]]
+    [:ul {:class "dropdown-menu"}
+     [:li [:a {:href "#/change-pass"} [:i {:class "nav-icon icon glyphicon glyphicon-refresh"}] "Change Password"]]
+     [:li {:class "divider"}]
+     [:li {:id "log-in-or-out" :on-click #(remove-local-storage! :auth-creds)} [:a {:href "#/"} [:i {:class "nav-icon icon glyphicon glyphicon-off"}] "Logout"]]]]])
+
+(defn auth-nav-not-logged-in []
   [:ul {:class "nav navbar-nav navbar-right"}
    [:li {:class "dropdown"}
     [:a {:class "dropdown-toggle" :data-toggle "dropdown"} "Login" [:b {:class "caret"}]]
@@ -25,17 +33,14 @@
      [:li {:class "divider"}]
      [:li {:id "log-in-or-out"} [:a {:href "#/login"} [:i {:class "nav-icon icon glyphicon glyphicon-off"}] "Login"]]]]])
 
-(let [username (:username @auth-creds-ratom)]
-  (if username
-    (.log js/console (str "We have user: " username))
-    (.log js/console "No user yet")))
+(defn auth-nav-desktop []
+  (if (:username @auth-creds-ratom)
+    [auth-nav-logged-in]
+    [auth-nav-not-logged-in]))
 
-(defn auth-nav-mobile []
+(defn auth-nav-mobile-not-logged-in []
   [:div
-   [:div {:class "mobile-menu-header"}
-    (if (not-empty (:example-token @global))
-      [:h3 "Logout"]
-      [:h3 "Login"])]
+   [:div {:class "mobile-menu-header"} [:h3 "Login"]]
    [:li {:class (str "mobile-menu-link " (active-route? "register"))}
     [:a {:href "#/register" :on-click #(mobile-nav-click "/register" "register")} "Register"]]
    [:li {:class (str "mobile-menu-link " (active-route? "lost-pass"))}
@@ -43,9 +48,16 @@
    [:li {:class (str "mobile-menu-link " (active-route? "login"))}
     [:a {:href "#/login" :on-click #(mobile-nav-click "/login" "login")} "Login"]]])
 
-; Mobile menu when we do have a token
-;[:li {:class "mobile-menu-link"} [:a {:href "#/" :on-click #(mobile-nav-click "/" "register")} "Change Password"]]
-;[:li {:class "mobile-menu-link"} [:a {:href "#/" :on-click #(mobile-nav-click "/" "register")} "Logout"]]
+(defn auth-nav-mobile-logged-in []
+  [:div
+    [:div {:class "mobile-menu-header"} [:h3 "Logout"]]
+    [:li {:class "mobile-menu-link"} [:a {:href "#/" :on-click #(mobile-nav-click "/" "register")} "Change Password"]]
+    [:li {:class "mobile-menu-link"} [:a {:href "#/" :on-click #(do (remove-local-storage! :auth-creds) (mobile-nav-click "/" "index"))} "Logout"]]])
+
+(defn auth-nav-mobile []
+  (if (:username @auth-creds-ratom)
+    [auth-nav-mobile-logged-in]
+    [auth-nav-mobile-not-logged-in]))
 
 (defn mobile-nav []
   [:div {:class "container"}
--- src/cljs/reagent_batteries_included_token_auth/shared_state.cljs
+++ src/cljs/reagent_batteries_included_token_auth/shared_state.cljs
@@ -1,24 +1,18 @@
 (ns reagent-batteries-included-token-auth.shared-state
-  (:require [alandipert.storage-atom :refer [local-storage remove-local-storage!]]
+  (:require [alandipert.storage-atom :refer [local-storage]]
             [reagent.core :as ratom]))
 
 (def auth-creds-ls (local-storage (atom {}) :auth-creds))
 (def auth-creds-ratom (ratom/atom {}))
-
-; (add-watch auth-creds-ls :state-maker
-;            (fn [k _ _ v]
-;              (.log js/console "Thing k: " k)
-;              (.log js/console "Thing v: " v)))
+(reset! auth-creds-ratom @auth-creds-ls)
 
 (add-watch auth-creds-ls :anything
   (fn [key atom old-state new-state]
     (reset! auth-creds-ratom new-state)))
 
-; (.log js/console @auth-creds-ratom)
-; (remove-local-storage! :auth-creds)
-
-; (swap! auth-creds assoc :user-token "777zzz")
-
+;; ==========
+;  Perhaps useful for debugging
+;; ==========
 ; (defn get-storage-atom-local-value [key-name]
 ;  (.getItem js/localStorage (str "[\"k\",\"" key-name "\"]")))
 
