3410189cad2f9a5da57a77da4b17336dd3332c45
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2015-06-29 22:04:19 -0500

    Enhancement: Login no longer hard coded uses input from user

--- src/cljs/reagent_batteries_included_token_auth/auth/login.cljs
+++ src/cljs/reagent_batteries_included_token_auth/auth/login.cljs
@@ -4,10 +4,11 @@
             [goog.crypt.base64 :as b64]
             [reagent-batteries-included-token-auth.shared-state :as ss]))
 
-(def password-state (ratom/atom {:password-visible false :ajax-info {}}))
+(def password-visible (ratom/atom false))
+(def credentials (ratom/atom {:username "" :password ""}))
 
 (defn toggle-visibility []
-  (swap! password-state assoc :password-visible (not (:password-visible @password-state))))
+  (reset! password-visible (not @password-visible)))
 
 (defn response-handler [response]
   (swap! ss/auth-creds-ls assoc :token (:token response))
@@ -20,23 +21,39 @@
 
 
 (defn auth-header []
-  (str "Basic " (b64/encodeString "Jarrod1031:P455word1")))
+  (str "Basic " (b64/encodeString (str (:username @credentials) ":" (:password @credentials)))))
 
 (defn attempt-login []
-  (GET "https://button-pusher-server.herokuapp.com/api/auth" {:headers       {"Authorization" (auth-header)}
-                                                              :handler       response-handler
-                                                              :error-handler error-handler
+  (GET "https://button-pusher-server.herokuapp.com/api/auth" {:headers         {"Authorization" (auth-header)}
+                                                              :handler         response-handler
+                                                              :error-handler   error-handler
                                                               :response-format :json
-                                                              :keywords? true
-                                                              :prefix true}))
+                                                              :keywords?       true
+                                                              :prefix          true}))
+
+(defn username-input-box []
+  [:input {:type        "text"
+           :id          "login-username"
+           :class       "form-control"
+           :value       (:username @credentials)
+           :placeholder "Username or Email"
+           :on-change   #(swap! credentials assoc :username (-> % .-target .-value))}])
+
+(defn password-input-box []
+  [:input {:type        (if @password-visible "text" "password")
+           :id          "login-password"
+           :class       "form-control"
+           :value       (:password @credentials)
+           :placeholder "Password"
+           :on-change   #(swap! credentials assoc :password (-> % .-target .-value))}])
 
 (defn login-page []
   [:div {:id "login-wrapper"}
-   [:h1 "Log In "]
-   [:input {:type "text" :id "login-username" :class "form-control" :placeholder "Username or Email"}]
-   [:input {:type (if (:password-visible @password-state) "text" "password") :id "login-password" :class "form-control" :placeholder "Password"}]
-   [:div {:id "pass-toggle" :class (str "glyphicon " (if (:password-visible @password-state) "glyphicon-eye-open" "glyphicon-eye-close")) :on-click #(toggle-visibility)}]
-   [:input {:type "button" :id "login-submit-btn" :class "btn btn-info btn-outline" :value "Log In" :on-click #(attempt-login)}]
-   [:div {:class "row login-links-row"}
-    [:div {:class "col-xs-6"} [:a {:href "google.com"} "Register"]]
-    [:div {:class "col-xs-6 forgot-pass-link"} [:a {:href "google.com"} "Forgot Password"]]]])
+    [:h1 "Log In "]
+    [username-input-box]
+    [password-input-box]
+    [:div {:id "pass-toggle" :class (str "glyphicon " (if @password-visible "glyphicon-eye-open" "glyphicon-eye-close")) :on-click #(toggle-visibility)}]
+    [:input {:type "button" :id "login-submit-btn" :class "btn btn-info btn-outline" :value "Log In" :on-click #(attempt-login)}]
+    [:div {:class "row login-links-row"}
+      [:div {:class "col-xs-6"} [:a {:href "google.com"} "Register"]]
+      [:div {:class "col-xs-6 forgot-pass-link"} [:a {:href "google.com"} "Forgot Password"]]]])
